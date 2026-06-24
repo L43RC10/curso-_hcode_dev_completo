665f72d3ae1c18a234c0c51221233e48d7b68dbc
Laercio Pereira <laercio3714@gmail.com>
2026-06-24 16:50:29 -0300

    curso HCode Dev Completo: exercicios

---
+++ 66. Criando Home da Loja - Parte 1/view/shop.php
@@ -0,0 +1,20 @@
+<?php include_once("header.php"); ?>
+
+		<section>
+
+			<div class="container">
+
+				<div id="destaque-produtos">
+					<div class="item">
+						
+						<div class="col-sm-6"></div>
+						<div class="col-sm-6"></div>
+
+					</div>
+				</div>
+
+			</div>
+
+		</section>
+
+		<?php include_once("footer.php");?>
